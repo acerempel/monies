19bb77809622369b01e410e30e85dab75398b4b4
Alan Rempel <alan.rempel@gmail.com>
2022-03-23 09:20:52 -0400

    Enable foreign key constraints on all connections

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ use std::ops::Deref;
 use axum::extract::Extension;
 use axum::{Router, routing, Json};
 use axum_macros::debug_handler;
-use r2d2::Pool;
+use r2d2::{Pool, CustomizeConnection};
 use r2d2_sqlite::SqliteConnectionManager;
-use rusqlite::Row;
+use rusqlite::{Row, Connection};
 use serde::Serialize;
 
 #[tokio::main]
 async fn main() -> Result<(), eyre::Report> {
-    let pool = Pool::new(SqliteConnectionManager::file("database.db"))?;
+    let pool = Pool::builder()
+        .connection_customizer(Box::new(ConnectionCustomizer))
+        .build(SqliteConnectionManager::file("database.db"))?;
     init_database(pool.get()?.deref())?;
     let app = Router::new()
         .route("/transactions", routing::get(list_transactions))
@@ -19,6 +21,19 @@ async fn main() -> Result<(), eyre::Report> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 4000));
     axum::Server::bind(&addr).serve(app.into_make_service()).await?;
     Ok(())
+}
+
+#[derive(Debug)]
+struct ConnectionCustomizer;
+
+impl CustomizeConnection<Connection, rusqlite::Error> for ConnectionCustomizer {
+    fn on_acquire(&self, conn: &mut Connection) -> Result<(), rusqlite::Error> {
+        conn.execute("PRAGMA foreign_keys = ON", [])?;
+        let mut res = 0;
+        conn.query_row("PRAGMA foreign_keys", [], |r| { res = r.get(0)?; Ok(()) })?;
+        assert_eq!(res, 1, "PRAGMA foreign_keys not supported");
+        Ok(())
+    }
 }
 
 #[debug_handler]
